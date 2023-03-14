825fae2210d769732ac9530c6f6587cb8c50972d
enes <enes.findik@gmx.de>
2023-03-14 07:22:59 +0100

    feat: add search page, WIP

---
+++ pages/search/[query].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+const query = useSearchQuery(route.params.query as string);
+await query.suspense();
+console.log(query.data);
+</script>
+
+<template>
+  <div>
+    {{ query.data }}
+  </div>
+</template>
